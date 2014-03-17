63bd08a18a81cfc0fb2896a6fc56ecbb20ef7084
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-03-17 07:44:44 +0000

    minor changes

--- System/Linux/CGroup.hs
+++ System/Linux/CGroup.hs
@@ -9,7 +9,8 @@
 module System.Linux.CGroup
     ( CGroup(..)
     , getRootCGroup
-    , getCGroup
+    , findCGroup
+    , listTasks
     ) where
 
 import System.Linux.CGroup.Types
@@ -17,13 +18,15 @@ import System.Linux.CGroup.Types
 import qualified Filesystem.Path.CurrentOS as FS
 import qualified Filesystem                as FS
 import qualified Data.ByteString.Char8     as BS
+import qualified Data.ByteString.UTF8      as BS (toString)
 import Data.Maybe
+import System.Posix.Types(ProcessID)
 
+-- | retrieve the mount point of the CGroup file system
 getRootCGroup :: IO (Maybe CGroup)
 getRootCGroup = do
     mountedContent <- readFile "/proc/mounts"
     let list = mapMaybe decodeLine $ lines mountedContent
-    putStrLn $ show list
     case list of
         []     -> return Nothing
         [a]    -> buildCGroupTree a
@@ -42,19 +45,29 @@ getRootCGroup = do
               subCGroups <- mapM buildCGroupTree listContents
               return $ Just $ CGroup path $ mapMaybe id subCGroups
 
-getCGroup :: CGroup -> FS.FilePath -> Maybe CGroup
-getCGroup group path
-    | cgroupPath group == path                 = Just group
-    | (FS.filename $ cgroupPath group) == path = Just group
-    | otherwise =
+-- | find a CGroup in the given CGroup Tree.
+findCGroup :: CGroup       -- ^ CGroup Tree ("/" is the main CGROUP)
+           -> FS.FilePath  -- ^ an absolute file path
+           -> Either String CGroup -- ^ maybe the finded CGroup
+findCGroup group path
+    | (FS.absolute path) =
         let splitedDir = FS.splitDirectories path
-        in  findCGroup [group] splitedDir
+        in  case splitedDir of
+                [_]   -> Right group
+                (a:b) -> findSubCGroup (subCGroups group) b
+                _     -> Left $ "unable to find path: " ++ (FS.encodeString path)
+    | otherwise = Left $ "filepath not supported: " ++ (FS.encodeString path)
     where
-      findCGroup :: [CGroup] -> [FS.FilePath] -> Maybe CGroup
-      findCGroup [] _ = Nothing
-      findCGroup _ [] = Nothing
-      findCGroup (cg:xs) [name] =
-          if (FS.filename $ cgroupPath cg) == name then Just cg else findCGroup xs [name]
-      findCGroup (cg:xs) (a:b) =
-          if (FS.filename $ cgroupPath cg) == a then findCGroup (subCGroups cg) b
-                                                else findCGroup xs (a:b)
+      findSubCGroup :: [CGroup] -> [FS.FilePath] -> Either String CGroup
+      findSubCGroup [] l = Left $ "not subCGroup named: " ++ (FS.encodeString $ FS.concat l)
+      findSubCGroup _ [] = Left "CGroup not found"
+      findSubCGroup (cg:xs) [name] =
+          if (FS.filename $ cgroupPath cg) == name then Right cg else findSubCGroup xs [name]
+      findSubCGroup (cg:xs) (a:b) =
+          if (FS.filename $ cgroupPath cg) == a then findSubCGroup (subCGroups cg) b
+                                                else findSubCGroup xs (a:b)
+
+listTasks :: CGroup -> IO [ProcessID]
+listTasks cg = do
+    content <- readFile $ FS.encodeString $ (cgroupPath cg) FS.</> "tasks"
+    return $ map (read) (lines content)
